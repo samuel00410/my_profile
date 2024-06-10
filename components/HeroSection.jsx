f9e66b850e50ebee708b2dd4bb430c41976a1d83
samuel00410 <samuel00410@gmail.com>
2024-06-10 16:58:14 +0800

    初始化建立next.js專案

---
+++ components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h1 className="text-white">你好, 我是 林毓翔 Samuel !</h1>
+    </section>
+  );
+};
+
+export default HeroSection;
