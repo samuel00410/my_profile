98e8038a3df4db43c349c19bd3537ac37e345bba
samuel00410 <samuel00410@gmail.com>
2024-06-11 16:32:54 +0800

    上傳頁首組件

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 
+// 頁首主區塊(組件)
 const HeroSection = () => {
   return (
     <section>
